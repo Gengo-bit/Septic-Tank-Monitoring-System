040a6ee3532f18ef8ca899c59b25a52c6d8ed621
Gengo-bit <paulcorsino28@gmail.com>
2024-10-15 22:17:44 +0800

    fixed

--- src/home.js
+++ src/home.js
@@ -1,3 +1,15 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgrcyyM547ICJc6fzbunqWSV64pKlRfZA",
+  authDomain: "septic-tank-capacity.firebaseapp.com",
+  databaseURL: "https://septic-tank-capacity-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "septic-tank-capacity",
+  storageBucket: "septic-tank-capacity.appspot.com",
+  messagingSenderId: "445055846573",
+  appId: "1:445055846573:web:166f5bcc5e6b8d40e6de24",
+  measurementId: "G-M9K3YTLTRP"
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
@@ -42,3 +54,25 @@ function updateUI(userData) {
   
   // Continue updating other parts of the UI as needed
 }
+
+    // Sidebar toggle
+    const sidebarToggle = document.getElementById('sidebar-toggle');
+    const sidebar = document.getElementById('sidebar');
+    const closeBtn = document.getElementById('close-btn');
+
+    sidebarToggle.addEventListener('click', () => {
+        sidebar.classList.toggle('sidebar-open');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        sidebar.classList.remove('sidebar-open');
+    });
+
+    // Add event listener to the Logout button
+    document.getElementById('logout-btn').addEventListener('click', function() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = 'index.html';
+    }).catch((error) => {
+        console.error('Logout Error: ', error);
+    });
+});
